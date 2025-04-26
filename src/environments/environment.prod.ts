d3041d93a3e55511f21074fe81431ff8c76f7ede
ozobianwadibia <ozobia.nwadibia@snhu.edu>
2025-04-25 20:54:35 -0500

    Modified main.ts and environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://av468w4x9j.execute-api.us-east-1.amazonaws.com'
 };
